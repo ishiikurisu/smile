accfc1cb0cef8cd3ae243bae45553ee9f44c6ac3
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-12-04 21:50:05 -0200

    Basic thing done

--- js/app.js
+++ js/app.js
@@ -1,62 +1,20 @@
-function pickForThem() {
-    var options = getOptions();
-    var element = document.getElementById('choice');
-    var index = Math.round((Math.random() * 100) % options.length);
-    var selected = options[index];
-
-    while (selected === undefined) {
-        index = Math.round((Math.random() * 100) % options.length);
-        selected = options[index];
-    }
-
-    element.innerHTML = selected.title;
+function thinkOfAReason() {
+    var selected = undefined;
+    for (var options = getOptions(); selected === undefined; selected = getRandomFromList(options));
+    return selected;
 }
 
 function getOptions() {
     return [
-        {
-            'title': 'macarrão',
-            'price': 30
-        },
-        {
-            'title': 'sushi',
-            'price': 40
-        },
-        {
-            'title': 'bomba',
-            'price': 20
-        },
-        {
-            'title': '\'burguer gourmet',
-            'price': 40
-        },
-        {
-            'title': 'australiano',
-            'price': 60
-        },
-        {
-            'title': 'coreano',
-            'price': 60
-        },
-        {
-            'title': 'self service',
-            'price': 20
-        },
-        {
-            'title': 'hot dog',
-            'price': 15
-        },
-        {
-            'title': 'chinês',
-            'price': 20
-        },
-        {
-            'title': 'o que você cozinhar',
-            'price': 10
-        },
-        {
-            'title': 'nada',
-            'price': 0
-        }
+        'Tem alguém neste planeta que te ama!',
+        'Você ainda tem que encher seu porquinho!'
     ]
 }
+
+function getRandomFromList(list) {
+    return list[Math.round((Math.random() * 100) % list.length)];
+}
+
+$(function() {
+    reason.innerHTML = thinkOfAReason();
+});
