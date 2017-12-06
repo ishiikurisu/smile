b9b8ca8ec2af7b79805d29ce57b21e4ee4c80ecc
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-12-06 21:51:54 -0200

    Adding more phrases

--- js/app.js
+++ js/app.js
@@ -13,7 +13,16 @@ function getOptions() {
         '<a href="https://gph.is/2cv3xQY">Este</a> GIF',
         'Tente fazer alguém feliz!',
         'Eu sei que você tem um pet fofo querendo atenção!',
-        'Você ainda tem que encher seu porquinho!'
+        'Você ainda tem que encher seu porquinho de moedas!',
+        'Seu pai é borracheiro? Porque você é uma graxinha!',
+        'O seu chuveiro gosta de ouvir seu show particular.',
+        'Já imaginou como é morar dentro de um cogumelo com os smurfs?',
+        'Sabe se tem alguém jogando quadribol na sua cidade?',
+        'Na teoria, sua idade é a mesma do universo, pois toda a matéria é a mesma desde o começo de tudo.',
+        'Já considerou caminhar a Te Araroa?',
+        'Acredite se quiser: os olhos de alguém brilham ao te ver.',
+        'Faça um Origami de borboleta para dar para alguém que você gosta.',
+        'E aquelas pessoas que são gratas por poder passar um tempinho contigo? '
     ]
 }
 
